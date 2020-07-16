ba19783f000ca92ffeb26a8b0fcd23570fd4c380
Amy Simpson <simpson.r.amy@gmail.com>
2020-07-16 17:42:40 +0000

    Automatically backed up by Learn

--- js/selectors.js
+++ js/selectors.js
@@ -3,7 +3,7 @@ function paragraphSelector(){
   return $('p');
 }
 
-
+//Write a function lastImageSelector that does not accept any parameters. The function should use the last jQuery selector to return the last image in index.html.
 function lastImageSelector(){
   return $('img:last');
 }
